23b7cbf3259b4f8d951fdc389c6c41fa301d3099
prabal nikkumbh <prabalnikkumbh123@gmail.com>
2021-11-20 11:39:14 +0530

    modified setInterval polyfill

--- setAll.js
+++ setAll.js
@@ -1,78 +1,78 @@
 // set timeout
-// function greet(){
-//     console.log("hello")
-// }
-// setTimeout(greet,2000);
+function greet(){
+    console.log("hello")
+}
+setTimeout(greet,2000);
 
-// console.log("bye")
+console.log("bye")
 
 // set interval 
 // executes the function in intervals
 
 
-// let counter = 0
+let counter = 0
 
-// function sayHi(){
-//     counter++
-//     console.log("hiii")
-//     if(counter>=3){
-//         clearInterval(intervalID)
-//     }
-// }
+function sayHi(){
+    counter++
+    console.log("hiii")
+    if(counter>=3){
+        clearInterval(intervalID)
+    }
+}
 
-// let intervalID = setInterval(sayHi,2000)
-// sayHi()
+let intervalID = setInterval(sayHi,2000)
+sayHi()
 
 // polyfill of set interval
 // this is the polyfill of setInterval
 
 
-// function createSetInterval() {
+function createSetInterval() {
 
-//     let intervalID = 0;
-//     let intervalMap = {}
+    let intervalID = 0;
+    let intervalMap = {}
 
-//     function setIntervalPolyfill(callback, delay = 0, ...args) {
-//         var id = intervalID++;
-//         function repeat() {
-//             intervalMap[id] = setTimeout(() => {
-//                 callback(...args)
+    function setIntervalPolyfill(callback, delay = 0, ...args) {
+        var id = intervalID++;
+        function repeat() {
+            intervalMap[id] = setTimeout(() => {
+                callback(...args)
 
-//                 if (intervalMap[id]) {
-//                     repeat()
-//                 }
-//             }, delay);
-//         }
-//         repeat()
-//         return id
-//     }
+                if (intervalMap[id]) {
+                    repeat()
+                }
+            }, delay);
+        }
+        repeat()
+        return id
+    }
 
-//     function clearIntervalPolyfill(intervalID) {
-//         clearTimeout(intervalMap[intervalID])
-//         delete intervalMap[intervalID]
-//     }
+    function clearIntervalPolyfill(intervalID) {
+        clearTimeout(intervalMap[intervalID])
+        delete intervalMap[intervalID]
+    }
 
-//     return {
-//         setIntervalPolyfill,
-//         clearIntervalPolyfill
-//     }
-// }
+    return {
+        setIntervalPolyfill,
+        clearIntervalPolyfill
+    }
+}
 
-// const {
-//     setIntervalPolyfill,
-//     clearIntervalPolyfill
-// } = createSetInterval()
+const {
+    setIntervalPolyfill,
+    clearIntervalPolyfill
+} = createSetInterval()
 
-// let counter = 0
-// let intervalid 
+let counter = 0
+let intervalid 
 
-// function  greeting() {
-//     counter++
-//     console.log("hiii")
+function  greeting() {
+    counter++
+    console.log("hiii")
 
-//     if(counter>=3){
-//         clearIntervalPolyfill(intervalid)
-//     }
-// }
+    if(counter>=3){
+        clearIntervalPolyfill(intervalid)
+    }
+}
 
-// intervalid = setIntervalPolyfill(greeting,2000)
+intervalid = setIntervalPolyfill(greeting,2000)
